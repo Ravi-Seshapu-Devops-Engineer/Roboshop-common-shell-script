2c665fdc5259d36b81d513d2ace08a3c52a4fcbf
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-05-03 19:34:56 +0530

    actions

--- redis.sh
+++ redis.sh
@@ -13,8 +13,8 @@ validate $? "Redis module disabled and module 7 enabled"
 dnf install $app_name -y &>>$LOGS_FILE
 validate $? "$app_name installation"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOGS_FILE
-validate $? "Allowing remote access"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
 
 systemctl enable redis &>>$LOGS_FILE
 systemctl start redis &>>$LOGS_FILE
